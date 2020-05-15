c472755af142294ba37b792d512db22a0ae5b177
Jakub Kalina <kalinakuba6@gmail.com>
2020-05-15 16:38:36 +0200

    Initial commit

---
+++ zad2/zad2.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+
+    let indexNumber = 241346;
+    let exerciseNumberResult = indexNumber % 5;
+    document.getElementById("exerciseNumberResult").innerHTML = exerciseNumberResult;
+
+});
