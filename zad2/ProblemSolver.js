a7a41b9bc7dc0c7d6b8ab0a3a85bea2a5192dea6
Jakub Kalina <kalinakuba6@gmail.com>
2020-05-15 19:59:24 +0200

    Added general user interface for exercise numer 1 and 2

---
+++ zad2/ProblemSolver.js
@@ -0,0 +1,12 @@
+class ProblemSolver {
+
+    // Konstruktor z parametrami
+    constructor(problemSize, arrayOfNumbers) {
+        this.problemSize = problemSize;
+        this.arrayOfNumbers = arrayOfNumbers;
+    }
+
+    solve() {
+
+    }
+}
--- zad2/zad2.js
+++ zad2/zad2.js
@@ -1,7 +1,28 @@
+let drawNumbersButton;
+let problemSize, minNumber, maxNumber;
+let arrayOfNumbers;
+
+
 document.addEventListener('DOMContentLoaded', (e) => {
+    drawNumbersButton = document.getElementById("drawArrayOfNumbers");
 
     let indexNumber = 241346;
     let exerciseNumberResult = indexNumber % 5;
     document.getElementById("exerciseNumberResult").innerHTML = exerciseNumberResult;
 
+    drawNumbersButton.addEventListener("click", drawArrayOfNumbers)
+    
+
 });
+
+// Funkcja generująca tablicę losowych liczb o zadanym rozmiarze
+function drawArrayOfNumbers() {
+    arrayOfNumbers = [];
+    problemSize = parseInt(document.getElementById("problemSizeValue").value);
+    minNumber = parseInt(document.getElementById("minNumberValue").value);
+    maxNumber = parseInt(document.getElementById("maxNumberValue").value);
+
+    for(let i = 0 ; i < problemSize; i++) {
+        arrayOfNumbers.push(Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber);
+    }
+}
