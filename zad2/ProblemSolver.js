3d77adc7e076662feb729bd2c18be902a9b33a15
Jakub Kalina <kalinakuba6@gmail.com>
2020-05-16 19:24:44 +0200

    Finished exercise number 2

--- zad2/ProblemSolver.js
+++ zad2/ProblemSolver.js
@@ -1,12 +1,95 @@
 class ProblemSolver {
 
     // Konstruktor z parametrami
-    constructor(problemSize, arrayOfNumbers) {
-        this.problemSize = problemSize;
+    constructor(arrayOfNumbers) {
         this.arrayOfNumbers = arrayOfNumbers;
     }
 
-    solve() {
+    solution(A) {
+        let lowestSum;
+        let ArrayLength = A.length;
+        let maxValue = 0;
 
+        for(let i = 0 ; i < ArrayLength; i++) {
+            A[i] = Math.abs(A[i]);
+            if(maxValue > A[i]) {
+                maxValue = maxValue;
+            } else {
+                maxValue = A[i];
+            }
+        }
+
+        let SumOfElements = 0;
+
+        for(let i = 0 ; i < A.length; i++) {
+            SumOfElements += A[i];
+        }
+
+        let countMultiple = [];
+
+        for(let i = 0 ; i < maxValue + 1; i++) {
+            countMultiple.push(0);
+        }
+
+        // Liczenie wystąpień
+        for(let i = 0 ; i < ArrayLength; i++) {
+            countMultiple[A[i]] += 1;
+        }
+
+        let tmp = [];
+
+        for(let i = 0 ; i < SumOfElements + 1; i++) {
+            tmp.push(-1);
+        }
+
+        tmp[0] = 0;
+        let counterStep = Math.floor(((maxValue + 1) * (SumOfElements)) / 100);
+        let counterTmp = 0;
+
+
+        for(let a = 1; a < maxValue + 1 ; a++) {
+            if(countMultiple[a] > 0){
+                for(let j = 0; j < SumOfElements; j++) {
+
+                    counterTmp++;
+                    if(counterTmp == counterStep) {
+                        postMessage({action: 'notifyProgress', status: 'done'});
+                        counterTmp = 0;
+                    }
+
+                    if(tmp[j] >= 0) {
+                        tmp[j] = countMultiple[a];
+                    }  
+                    if((j >= a) && (tmp[j-a] > 0)) {
+                        tmp[j] = tmp[j-a] -1;
+                    }
+                }
+            }
+        }
+
+        let result = SumOfElements;
+
+        for(let i = 0 ; i < (SumOfElements / 2) + 1; i++ ) {
+            if(tmp[i] >= 0) {
+                if(result < SumOfElements - 2 * i) {
+                    result = result;
+                } else {
+                    result = SumOfElements - 2 * i;
+                }
+            }
+        }
+
+        lowestSum = result;
+
+        //     postMessage({action: 'notifyProgress', status: 'done'});
+
+        postMessage({action: 'finishedCalculations', lowestSum: lowestSum});
     }
 }
+
+addEventListener('message', ({data}) => {
+    if(data.action == 'startCalculations') {
+        let solver = new ProblemSolver(data.arrayOfNumbers);
+        solver.solution(solver.arrayOfNumbers);
+    }
+});
--- zad2/zad2.js
+++ zad2/zad2.js
@@ -1,18 +1,25 @@
-let drawNumbersButton;
+let drawNumbersButton, beginSolverButton, statusProgressBar, resultsBlock, lowestSumResult;
 let problemSize, minNumber, maxNumber;
 let arrayOfNumbers;
-
+let solverWorker;
+let progressBarValue = 0;
+let  bestSequence;
+let lowestSum;
 
 document.addEventListener('DOMContentLoaded', (e) => {
     drawNumbersButton = document.getElementById("drawArrayOfNumbers");
+    beginSolverButton = document.getElementById("beginSolverButton");
+    statusProgressBar = document.getElementById("statusProgressBar");
+    resultsBlock = document.getElementById("resultsBlock");
+    lowestSumResult = document.getElementById("lowestSumResult");
 
     let indexNumber = 241346;
     let exerciseNumberResult = indexNumber % 5;
     document.getElementById("exerciseNumberResult").innerHTML = exerciseNumberResult;
 
-    drawNumbersButton.addEventListener("click", drawArrayOfNumbers)
+    drawNumbersButton.addEventListener("click", drawArrayOfNumbers);
+    beginSolverButton.addEventListener("click", beginSolverCalculations);
     
-
 });
 
 // Funkcja generująca tablicę losowych liczb o zadanym rozmiarze
@@ -21,8 +28,50 @@ function drawArrayOfNumbers() {
     problemSize = parseInt(document.getElementById("problemSizeValue").value);
     minNumber = parseInt(document.getElementById("minNumberValue").value);
     maxNumber = parseInt(document.getElementById("maxNumberValue").value);
+    beginSolverButton.disabled = false;
+
+    // Aktualizacja statusu
+    progressBarValue = 0;
+    statusProgressBar.innerHTML = progressBarValue + '%';
+    statusProgressBar.setAttribute('aria-valuenow', progressBarValue);
+    statusProgressBar.setAttribute('style', 'width: '+ progressBarValue +'%');
 
     for(let i = 0 ; i < problemSize; i++) {
         arrayOfNumbers.push(Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber);
     }
+
+}
+
+function beginSolverCalculations() {
+    solverWorker = new Worker('ProblemSolver.js');
+    solverWorker.postMessage({type: 'cmd', action: 'startCalculations', problemSize: problemSize, arrayOfNumbers: arrayOfNumbers});
+
+    solverWorker.addEventListener("message", ({data}) => {
+
+        // Jeśli aktualizacja paska postępu
+        if(data.action == 'notifyProgress') {
+
+            progressBarValue += 1;
+
+            // Aktualizacja statusu
+            statusProgressBar.innerHTML = progressBarValue + '%';
+            statusProgressBar.setAttribute('aria-valuenow', progressBarValue);
+            statusProgressBar.setAttribute('style', 'width: '+ progressBarValue +'%');
+        }
+    
+        // Jeśli ukończono obliczenia
+        if(data.action == 'finishedCalculations') {
+
+            progressBarValue = 100;
+
+            // Aktualizacja statusu
+            statusProgressBar.innerHTML = progressBarValue + '%';
+            statusProgressBar.setAttribute('aria-valuenow', progressBarValue);
+            statusProgressBar.setAttribute('style', 'width: '+ progressBarValue +'%');
+
+            lowestSumResult.innerHTML = data.lowestSum;
+            resultsBlock.setAttribute('style', 'display: block !important');
+        }
+
+    });
 }
